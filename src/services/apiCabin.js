ce8b914ff618976f4f6adc347d6b4156a333a76d
Daniel gakeri <danielgakeri@192.168.1.20>
2025-09-03 11:22:15 +0300

    added duplicate cabins

--- src/services/apiCabin.js
+++ src/services/apiCabin.js
@@ -37,6 +37,8 @@ const hasImagePath = typeof newCabin.image === 'string' && newCabin.image.starts
     throw new Error("Cabins could not be created");
   }
   // 2. Upload image
+
+  if (hasImagePath) return data;
   const { error: storageError } = await supabase
     .storage
     .from('cabin-images')
